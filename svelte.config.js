7632d9160150f1e04402000802b69df6aaf242fd
Tanish Gambhir <tanishgambhir55@gmail.com>
2025-06-09 01:50:40 +0530

    Changed some settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+const dev = process.env.NODE_ENV === 'development';
+
+export default {
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/your-repo-name'
+    }
+  }
 };
-
-export default config;
